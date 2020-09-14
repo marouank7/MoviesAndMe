51332c21232c386bee4579d44041076e29e60881
Marouan Khatab <marouan.khatab@domintell.com>
2020-09-14 16:22:04 +0200

    Api film + chargement

---
+++ API/TMDBApi.js
@@ -0,0 +1,27 @@
+// import axios from 'axios'
+// const API_TOKEN = "4a2ac6b804001d7a092fae28d9546053"
+
+// export function getFilmsFromApiWithSearchedText (text) {
+//     //const url = 'https://api.themoviedb.org/3/movie/550?api_key=ff0e57f87d1fa045bd18884993ed9705'
+//     console.log("je suis dans getFilmsFromApiWithSearchedText")
+//     const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+//     return axios.get(url)
+//         .then((reponse) => {
+//             reponse.json()
+//             console.log("response", reponse.data.results)
+//         })
+//         .catch((error) => console.log("error fetch",error))
+// }
+
+const API_TOKEN = "4a2ac6b804001d7a092fae28d9546053";
+
+export function getFilmsFromApiWithSearchedText (text) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+}
+
+export function getImageFromApi (name) {
+    return 'https://image.tmdb.org/t/p/w300' + name
+}
---
+++ Components/Search.js
@@ -0,0 +1,93 @@
+// Components/Search.js
+
+import React from 'react'
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native'
+import FilmItem from './FilmItem'
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
+
+class Search extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.searchedText = "" // Initialisation de notre donnée searchedText en dehors du state
+        this.state = {
+            films: [],
+            isLoading: false
+        }
+    }
+
+    _displayLoading(){
+        if(this.state.isLoading){
+            return(
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size="large"/>
+                </View>
+            )
+        }
+    }
+
+
+    _loadFilms() {
+        this.setState({isLoading : true})
+        if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
+        getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
+                this.setState({
+                    films: data.results,
+                    isLoading: false
+                })
+        })
+        }
+    }
+
+    _searchTextInputChanged(text) {
+        this.searchedText = text // Modification du texte recherché à chaque saisie de texte, sans passer par le setState comme avant
+    }
+
+    render() {
+        console.log("RENDER")
+        console.log("this.state.isLoading", this.state.isLoading)
+        return (
+        <View style={styles.main_container}>
+            <TextInput
+                style={styles.textinput}
+                placeholder='Titre du film'
+                onChangeText={(text) => this._searchTextInputChanged(text)}
+                onSubmitEditing={() => this._loadFilms()}
+            />
+            <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+            <FlatList
+            data={this.state.films}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({item}) => <FilmItem film={item}/>}
+            />
+            {this._displayLoading()}
+        </View>
+        )
+    }
+    }
+
+    const styles = StyleSheet.create({
+    main_container: {
+        flex: 1,
+        marginTop: 20
+    },
+    textinput: {
+        marginLeft: 5,
+        marginRight: 5,
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        paddingLeft: 5
+    },
+    loading_container:{
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 100,
+        bottom: 0,
+        alignItems: "center",
+        justifyContent: "center"
+    }
+    })
+
+export default Search
